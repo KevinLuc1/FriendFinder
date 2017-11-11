c9c255c09dc7023bbaf9aa92d0aa5700ff62e20d
KevinLuc1 <kevinluc1993@gmail.com>
2017-11-10 18:11:55 -0800

    workong on .post

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Rex',
+		photo: 'http://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development-300x200.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mittens',
+		photo: 'https://www.severnedgevets.co.uk/sites/default/files/styles/medium/public/guides/kitten.png?itok=K4YoP1wI',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Parry',
+		photo: 'https://i.pinimg.com/originals/2e/7a/17/2e7a177fad261ae6148817a92cf88414.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Rocky',
+		photo: 'http://www.zooborns.com/.a/6a010535647bf3970b01b8d07deb2e970c-800wi',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Sloopy',
+		photo: 'https://i.pinimg.com/736x/b8/2e/ae/b82eae0856b66d0d29e70e9d6021ace4--costarica-too-cute.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Giffy',
+		photo: 'http://www.denverpost.com/wp-content/uploads/2017/03/denverzoo-org-baby_giraffe-dobby_02-e1488394417321.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Oinky',
+		photo: 'https://i.ytimg.com/vi/1LqyOX12_nQ/maxresdefault.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Chad',
+		photo: 'http://yesserver.space.swri.edu/yes2013/personal/emilyklotzbach/panda2.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jeff',
+		photo: 'https://ichef.bbci.co.uk/images/ic/640x360/p04rkb53.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Sheeeila',
+		photo: 'https://redtricom.files.wordpress.com/2014/03/lamb-windrush.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
